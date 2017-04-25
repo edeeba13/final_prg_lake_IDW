b823afaf42b064b96afa1ade9eed2b039f32ac52
Emily Deeba <deeba@slu.edu>
2017-04-25 09:15:49 -0500

    This script includes a possible solution to my probelm that is commented out.

--- Final_Project_XY_Coords.py
+++ Final_Project_XY_Coords.py
@@ -12,17 +12,20 @@ import arcpy
 arcpy.env.overwriteOutput = True
 from arcpy import env
 import os
+arcpy.CheckOutExtension("Spatial")
+from arcpy.sa import *
+
 
 env.workspace = r"E:\P in GIS\Final project"
 output_fc = r"E:\P in GIS\Final project\Carlyle.gdb"
-input_data = r"E:\P in GIS\Final project\ALL_LAKE_DATA"
+input_data = r"E:\P in GIS\Final project\ALL_LAKE_DATA_2.csv"
 arcpy.CopyRows_management(input_data, output_fc)
 
 #arcpy.MakeXYEventLayer_management.py
 
 print "copying rows"
 
-in_Table = r"E:\P in GIS\Final project\ALL_LAKE_DATA"
+in_Table = input_data
 x_Coords = "Long"
 y_Coords = "Lat"
 out_Layer = "Carlyle_layer"
@@ -32,7 +35,7 @@ print "Creating XY Layer"
 arcpy.MakeXYEventLayer_management(in_Table, x_Coords, y_Coords, out_Layer)
 
 
-outLocation = r"E:\P in GIS\Final project\Carlyle.gdbb"
+outLocation = r"E:\P in GIS\Final project\Carlyle.gdb"
 outFeatureClass = "XYCoord"
 ##Changed inFeature to out_layer here
 
@@ -45,16 +48,37 @@ in_dataset = r"E:\P in GIS\Final project\Carlyle.gdb\XYCoord"
 coord_sys = arcpy.SpatialReference('NAD 1983 StatePlane Missouri East FIPS 2401 (US Feet)')
 ##not sure what spatial reference to use
 
-print "Defining Projection"
 
+out_gdb = r"E:\P in GIS\Final project\Carlyle.gdb"
+
+#I looked into the issue a bit, and I found that I may need to convert my excel spreadsheet to a table in order to access the data for the interpolation-- what do you this of this?
+##def importallsheets(input_data,out_gdb):
+##    workbook = xlrd.open_workbook(input_data)
+##    sheets = ["May vs June" for sheet in workbook.sheets()]
+##    print('{} sheets found: {}'.format(len(sheets), ','.join(sheets)))
+##    for sheet in sheets:
+##        # The out_table is based on the input excel file name
+##        # a underscore (_) separator followed by the sheet name
+##        out_table = os.path.join(out_gdb,arcpy.ValidateTableName("{0}_{1}".format(os.path.basename(input_data), sheet),
+##                out_gdb))
+##
+##        print('Converting {} to {}'.format(sheet, out_table))
+##
+##print "Performing the conversion"
+##
+##arcpy.ExcelToTable_conversion(in_excel, out_table, sheet)
+
+
+print "Defining Projection"
 arcpy.DefineProjection_management(in_dataset,coord_sys)
 
-import arcpy
-from arcpy import env
-from arcpy.sa import *
 
+in_Feature = "TP mg/kg"
+field_name = "TP_mg_kg"
 
-inPointFeatures = r"E:\P in GIS\Final project\Carlyle.lyr"
+arcpy.AddField_management(input_data, field_name, "LONG")
+
+inPointFeatures = in_dataset
 zField = "TP_mg_kg"
 cellSize = 2000.0
 power = 2
@@ -69,6 +93,12 @@ outIDW = Idw(inPointFeatures, zField, cellSize, power, searchRadius)
 print "saving"
 outIDW.save("E:\P in GIS\Final project\output")
 
+
+
+#interpolate points
+
+##from arcgis.features.analyze_patterns import interpolate_points
+##interpolated_rf = interpolate_points(rainfall, field='RAINFALL')
 #datetime plotting
 ##date_values = {'date': ['2010/03', '2011/04', '2012/12', '2014/01', '2015/02'],
 ##               'value': [12, 23, 20, 31, 42]}
