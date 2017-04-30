64eeb1faf0bd7005cf2ad239e39dadf8175060e5
Emily Deeba <deeba@slu.edu>
2017-04-30 14:19:11 -0500

    I've cleaned up my code and made some changes. I'm hoping to clip my interpolation to a carlyle shapefile I have to make it more clear, but I'm having trouble with it.

--- Final_Project_XY_Coords.py
+++ Final_Project_XY_Coords.py
@@ -16,13 +16,14 @@ arcpy.CheckOutExtension("Spatial")
 from arcpy.sa import *
 
 
-env.workspace = r"C:\Student\EMILY\final_prg"
-output_fc = r"C:\Student\EMILY\test.gdb"
-input_data = r"C:\Student\EMILY\final_prg\ALL_LAKE_DATA.csv"
+env.workspace = r"E:\P in GIS\Final project"
+output_fc = r"E:\P in GIS\Final project\Carlyle.gdb"
+input_data = r"E:\P in GIS\Final project\ALL_LAKE_DATA_2.csv"
+
 #define projection
 in_dataset = os.path.join(output_fc, "XYCoord")
-coord_sys = arcpy.SpatialReference('NAD 1983 StatePlane Missouri East FIPS 2401 (US Feet)')
-##not sure what spatial reference to use
+coord_sys = arcpy.SpatialReference('NAD 1983 UTM Zone 16N')
+
 
 arcpy.CopyRows_management(input_data, output_fc)
 
@@ -36,13 +37,14 @@ y_Coords = "Lat"
 out_Layer = "Carlyle_layer"
 saved_Layer = r"E:\P in GIS\Final project\Carlyle.lyr"
 
+out_raster = "Carlyle_raster.tif"
+
 print "Creating XY Layer"
 arcpy.MakeXYEventLayer_management(in_Table, x_Coords, y_Coords, out_Layer)
 
 
 outLocation = output_fc
 outFeatureClass = "XYCoord"
-##Changed inFeature to out_layer here
 
 print "Creating output feature class"
 
@@ -52,23 +54,6 @@ arcpy.FeatureClassToFeatureClass_conversion(out_Layer, outLocation,outFeatureCla
 
 
 out_gdb = r"E:\P in GIS\Final project\Carlyle.gdb"
-
-#I looked into the issue a bit, and I found that I may need to convert my excel spreadsheet to a table in order to access the data for the interpolation-- what do you this of this?
-##def importallsheets(input_data,out_gdb):
-##    workbook = xlrd.open_workbook(input_data)
-##    sheets = ["May vs June" for sheet in workbook.sheets()]
-##    print('{} sheets found: {}'.format(len(sheets), ','.join(sheets)))
-##    for sheet in sheets:
-##        # The out_table is based on the input excel file name
-##        # a underscore (_) separator followed by the sheet name
-##        out_table = os.path.join(out_gdb,arcpy.ValidateTableName("{0}_{1}".format(os.path.basename(input_data), sheet),
-##                out_gdb))
-##
-##        print('Converting {} to {}'.format(sheet, out_table))
-##
-##print "Performing the conversion"
-##
-##arcpy.ExcelToTable_conversion(in_excel, out_table, sheet)
 
 
 print "Defining Projection"
@@ -82,8 +67,8 @@ field_name_double = "TP_mg_kg_num"
 #this needs to be in_dataset
 arcpy.AddField_management(in_dataset, field_name_double, "DOUBLE")
 
-##------------
-#update that field
+
+#update field from string to float
 fields = [field_name_string, field_name_double]
 
 # Create update cursor for feature class
@@ -99,34 +84,24 @@ with arcpy.da.UpdateCursor(in_dataset, fields) as cursor:
         cursor.updateRow(row)
 
 inPointFeatures = in_dataset
-zField = field_name_double#"TP_mg_kg"
-cellSize = 2000.0
-power = 2
-searchRadius = RadiusVariable(10, 150000)
+zField = field_name_double
+cellSize = "6.49000000000115E-04"
+cellPower = 2
 
 # Execute IDW
 print "executing interpolation"
-outIDW = Idw(inPointFeatures, zField, cellSize, power, searchRadius)
+outIDW = Idw(inPointFeatures, zField, cellSize, cellPower)
 
 # Save the output
 print "saving"
-outIDW.save("raster1.tif")
+outIDW.save(out_raster)
+
+#preparing files to be clipped
+out_points = r"E:\P in GIS\Final project\output\Carlyle_points.shp"
+arcpy.RasterToPoint_conversion(out_raster, out_points)
+#clipping interpolation
+clip_features = r"E:\P in GIS\Final project\carl_poly.shp"
+out_Carlyle_poly = r"E:\P in GIS\Final project\output\clipped_carlyle_poly.shp"
+arcpy.Clip_analysis(out_points, clip_features, out_Carlyle_poly)
 
 
-
-#interpolate points
-
-##from arcgis.features.analyze_patterns import interpolate_points
-##interpolated_rf = interpolate_points(rainfall, field='RAINFALL')
-#datetime plotting
-##date_values = {'date': ['2010/03', '2011/04', '2012/12', '2014/01', '2015/02'],
-##               'value': [12, 23, 20, 31, 42]}
-##
-###transform date from string into datetime format. This is important!
-##from datetime import datetime
-##date_values['date'] = map(lambda x: datetime.strptime(x, '%Y/%m'), date_values['date'])
-##fig = plt.figure(figsize=(10,5))
-##plt.plot_date(date_values['date'], date_values['value'], 'bs-')
-##plt.title('Money on Account No.1', fontsize=16)
-##plt.xlabel('Month/Year', fontsize=16)
-##plt.ylabel('US$', fontsize=16)
